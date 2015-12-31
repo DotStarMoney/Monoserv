fe602d5c2f9c1c4dad0e39a9ff4039644efe1c7f
Christopher Brown <Batterybat@gmail.com>
2015-12-31 13:39:58 -0500

    Fisher-Price my first commit.

---
+++ main.cpp
@@ -0,0 +1,16 @@
+/*
+ * main.cpp
+ *
+ *  Created on: Dec 31, 2015
+ *      Author: ubuntu
+ */
+#include <iostream>
+
+int main(int argc, char** argv)
+{
+	std::cout << "green bens." << std::endl;
+	return 0;
+}
+
+
+
